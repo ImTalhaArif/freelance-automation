3e7d67acde2d340cb67ac246c2bcb22150ca5081
ImTalhaArif <62965957+ImTalhaArif@users.noreply.github.com>
2025-11-12 02:59:15 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 // app/layout.tsx
 import "./globals.css";
-import Header from "./components/Header";
+import Header from "../components/Header";
 import type { ReactNode } from "react";
 
 export const metadata = {
